8d520f497fbcd03f46a0904cf9db1f59cb817dba
gardenkim <oyasumiee2@gmail.com>
2024-03-11 01:40:53 +0900

    Feat: Implement search page

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,7 +1,29 @@
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { searchTermState, totalResultsState } from "../../atoms/searchState";
 import ContentTable from "../../components/ContentTable";
 import SearchForm from "../../components/Search/SearchForm";
 
 const SearchPage = (): JSX.Element => {
+  const setSearchTerm = useSetRecoilState(searchTermState);
+  const totalResults = useRecoilValue(totalResultsState);
+  const [showAlert, setShowAlert] = useState(false);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const query = queryParams.get("query");
+
+    if (query !== null) {
+      setSearchTerm(decodeURIComponent(query));
+    }
+
+    // 검색어가 없는 경우에만 알림을 보여줌
+    setShowAlert(query === null);
+  }, [location, setSearchTerm]);
+
   return (
     <div className="p-8 md:p-12 lg:px-16 lg:py-24">
       <div className="mx-auto max-w-l text-center">
@@ -9,9 +31,11 @@ const SearchPage = (): JSX.Element => {
           찾고 싶은 약을 검색해 보세요!
         </h2>
         <SearchForm />
-        <p className="mt-12 mb-20">총 20개의 검색 결과가 있습니다.</p>
+        <p className="mt-12 mb-20">
+          총 {totalResults}개의 검색 결과가 있습니다.
+        </p>
       </div>
-    <ContentTable />
+      <ContentTable />
     </div>
   );
 };
