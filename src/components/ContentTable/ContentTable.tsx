4b14e8c5a76bb88b5b46d1b9e24b036996183383
gardenkim <oyasumiee2@gmail.com>
2024-03-11 01:36:42 +0900

    Feat: Add search result function

--- src/components/ContentTable/ContentTable.tsx
+++ src/components/ContentTable/ContentTable.tsx
@@ -1,8 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { fetchDrugs } from "../../api/search";
+import Pagination from "./Pagination";
+import { searchTermState, totalResultsState } from "../../atoms/searchState";
+import { Drug } from "../../types/Drug"; 
 import { Link } from "react-router-dom";
 
-const ContentTable = (): JSX.Element => {
+const ContentTable = () => {
+  const searchTerm = useRecoilValue(searchTermState);
+  const setTotalResults = useSetRecoilState(totalResultsState);
+  const [drugs, setDrugs] = useState<Drug[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetchDrugs(currentPage, searchTerm);
+      setDrugs(response.drugs.data);
+      setCurrentPage(response.drugs.current_page);
+      setTotalPages(response.drugs.last_page);
+      setTotalResults(response.drugs.total_page);
+    };
+
+    if (searchTerm) {
+      fetchData();
+    }
+  }, [searchTerm, currentPage]);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
   return (
-    <div className="overflow-x-auto">
+<div className="overflow-x-auto mx-auto max-w-[75%]">
+      {" "}
       <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
         <thead className="ltr:text-left rtl:text-right">
           <tr>
@@ -13,39 +44,34 @@ const ContentTable = (): JSX.Element => {
               이름
             </th>
             <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-              간략 설명
+              설명
             </th>
           </tr>
         </thead>
-
-        <tbody className="divide-y divide-gray-200 ">
-          <tr className="odd:bg-white">
-            <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-              John Doe
-            </td>
-            <td className="whitespace-nowrap px-4 py-2 text-gray-700 cursor-pointer hover:underline">
-              <Link to="/detail">까스활명수</Link>
-            </td>
-            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-              Web Developer
-            </td>
-          </tr>
-        </tbody>
-
-        <tbody className="divide-y divide-gray-200 ">
-          <tr className="odd:bg-white">
-            <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-              John Doe
-            </td>
-            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-              24/05/1995
-            </td>
-            <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-              Web Developer
-            </td>
-          </tr>
+        <tbody className="divide-y divide-gray-200 text-center">
+          {drugs.map((drug) => (
+            <tr className="odd:bg-white" key={drug.id}>
+              <td className="whitespace-nowrap px-2 py-4 font-medium text-gray-900">
+                {drug.item_seq}
+              </td>
+              <td className="whitespace-nowrap px-8 py-4 text-gray-700 cursor-pointer hover:underline">
+                <Link to={`/detail/${drug.item_seq}`}>{drug.item_name}</Link>
+              </td>
+              {/* 10자 까지만 */}
+              <td className="whitespace-nowrap px-2 py-4 text-gray-700">
+                {drug.efcy_qesitm.length > 10
+                  ? `${drug.efcy_qesitm.substring(0, 20)}...`
+                  : drug.efcy_qesitm}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={handlePageChange}
+      />
     </div>
   );
 };
