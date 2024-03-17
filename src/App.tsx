6eb4d2a06a0df90a0b5b9fb199b48b7551074bd0
gardenkim <oyasumiee2@gmail.com>
2024-03-18 00:54:15 +0900

    Feat: Modify routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route, Outlet } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
 import MainPage from "./pages/MainPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-import Header from "./components/Header";
 import SearchPage from "./pages/SearchPage";
 import DetailPage from "./pages/DetailPage";
 import MyPage from "./pages/Mypage/MyPage";
+import MedicationPage from "./pages/Mypage/MedicationPage";
+import MyReviewPage from "./pages/Mypage/MyReviewPage";
+import OldMedicationPage from "./pages/Mypage/OldMedicationPage";
+import PrivacyPage from "./pages/Privacy/PrivacyPage";
 
 function App(): JSX.Element {
   return (
@@ -18,7 +22,14 @@ function App(): JSX.Element {
         <Route path="search" element={<SearchPage />} />
         <Route path="detail/:item_seq" element={<DetailPage />} />
       </Route>
-      <Route path="/mypage/:id" element={<MyPage />} />
+
+      <Route path="mypage/:id/*" element={<MyPageLayout />}>
+        <Route index element={<MyPage />} />
+        <Route path="medication" element={<MedicationPage />} />
+        <Route path="myreview" element={<MyReviewPage />} />
+        <Route path="oldmedication" element={<OldMedicationPage />} />
+        <Route path="privacy" element={<PrivacyPage />} />
+      </Route>
     </Routes>
   );
 }
@@ -30,6 +41,10 @@ function LayoutWithHeader(): JSX.Element {
       <Outlet />
     </>
   );
+}
+
+function MyPageLayout(): JSX.Element {
+  return <Outlet />;
 }
 
 export default App;
