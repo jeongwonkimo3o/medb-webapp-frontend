76ae789670b77c9726389fbd6b45723a7f472bfa
gardenkim <oyasumiee2@gmail.com>
2024-03-18 00:57:57 +0900

    Chore: Remove unnecessary menu items from the side menu

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -1,6 +1,9 @@
-import medb_logo from "../../assets/medb_logo.png";
+import { Link } from "react-router-dom";
+import medb_logo from "../../assets/medb_logo.png"; // 이미지 경로는 실제 이미지 경로로 변경해야 합니다.
 
-const SideMenu = (): JSX.Element => {
+const SideMenu = () => {
+  const id = localStorage.getItem("id");
+
   return (
     <div className="flex h-screen flex-col justify-between border-e bg-white w-64">
       <div className="px-4 py-6">
@@ -8,12 +11,12 @@ const SideMenu = (): JSX.Element => {
 
         <ul className="mt-6 space-y-1">
           <li>
-            <a
-              href="#"
+            <Link
+              to={`/mypage/${id}`}
               className="block rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700"
             >
               마이페이지 홈
-            </a>
+            </Link>
           </li>
 
           <li>
@@ -39,92 +42,51 @@ const SideMenu = (): JSX.Element => {
 
               <ul className="mt-2 space-y-1 px-4">
                 <li>
-                  <a
-                    href="#"
+                  <Link
+                    to={`/mypage/${id}/medication`}
                     className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
                   >
                     현재 복용 중인 약
-                  </a>
+                  </Link>
                 </li>
 
                 <li>
-                  <a
-                    href="#"
+                  <Link
+                    to={`/mypage/${id}/oldmedication`}
                     className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
                   >
                     과거 복용 약
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </details>
           </li>
 
           <li>
-            <a
-              href="#"
+            <Link
+              to={`/mypage/${id}/myreview`}
               className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
             >
               작성한 리뷰
-            </a>
+            </Link>
           </li>
 
           <li>
-            <a
-              href="#"
+            <Link
+              to={`/mypage/${id}/privacy`}
               className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
             >
-              메모 작성
-            </a>
+              개인 정보 변경
+            </Link>
           </li>
 
-          <li>
-            <details className="group [&_summary::-webkit-details-marker]:hidden">
-              <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
-                <span className="text-sm font-medium"> 계정 관리 </span>
-
-                <span className="shrink-0 transition duration-300 group-open:-rotate-180">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </span>
-              </summary>
-
-              <ul className="mt-2 space-y-1 px-4">
-                <li>
-                  <a
-                    href="#"
-                    className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
-                  >
-                    개인 정보 변경
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    href="#"
-                    className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
-                  >
-                    회원 탈퇴
-                  </a>
-                </li>
-              </ul>
-            </details>
-          </li>
+         
         </ul>
       </div>
 
       <div className="sticky inset-x-0 bottom-0 border-t border-gray-100">
-        <a
-          href="#"
+        <Link
+          to="/" // 로그아웃
           className="flex items-center gap-2 bg-white p-4 hover:bg-gray-50"
         >
           <div>
@@ -134,7 +96,7 @@ const SideMenu = (): JSX.Element => {
               <span> eric@frusciante.com </span>
             </p>
           </div>
-        </a>
+        </Link>
       </div>
     </div>
   );
