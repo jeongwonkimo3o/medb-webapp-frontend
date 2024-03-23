6cd5734eb7d354787bf914fb3bf104a24f136d38
gardenkim <oyasumiee2@gmail.com>
2024-03-23 22:30:22 +0900

    Chore: Remove React.StrictMode.

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
@@ -6,11 +5,9 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <Router>
-        <App />
-      </Router>
-    </RecoilRoot>
-  </React.StrictMode>
+  <RecoilRoot>
+    <Router>
+      <App />
+    </Router>
+  </RecoilRoot>
 );
